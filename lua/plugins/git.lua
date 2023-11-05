8ee50af4511d54ac2323e2cf0c235c3ab006b182
0x0003 <->
2023-11-05 15:16:00 +0900

    Add more `gitsigns` keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,13 +11,13 @@ ng.setup({
       ['g?'] = 'HelpPopup',
     },
     status = {
-      ['<leader>gs'] = 'Close',
+      ['<leader>r'] = 'Close',
       ['='] = 'Toggle',
     }
   }
 })
 
-kmap('n', '<leader>gs', function() ng.open({ kind = 'replace' }) end)
+kmap('n', '<leader>r', function() ng.open({ kind = 'replace' }) end)
 
 require('diffview').setup({
   use_icons = false,
@@ -34,12 +34,28 @@ gs.setup ({
     topdelete = { text = '^' },
     changedelete = { text = '~' },
   },
+  _signs_staged_enable = true,
+  _signs_staged = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '-' },
+    topdelete = { text = '^' },
+    changedelete = { text = '~' },
+  },
   attach_to_untracked = false,
   on_attach = function(bufnr)
+    kmap('n', '<leader>gs', gs.stage_hunk)
+    kmap('n', '<leader>gu', gs.undo_stage_hunk)
+    kmap('n', '<leader>gr', gs.reset_hunk)
+    kmap('n', '<leader>gS', gs.stage_buffer)
+    kmap('n', '<leader>gR', gs.reset_buffer)
+    kmap('n', '<leader>gh', gs.preview_hunk, { buffer = bufnr })
+    kmap('n', '<leader>gb', function() gs.blame_line { full = true } end)
+    kmap('n', '<leader>gB', gs.toggle_current_line_blame)
+    kmap('n', '<leader>gd', gs.diffthis)
+    kmap('n', '<leader>gD', function() gs.diffthis('~') end)
     kmap('n', '[g', gs.prev_hunk, { buffer = bufnr })
     kmap('n', ']g', gs.next_hunk, { buffer = bufnr })
-    kmap('n', '<leader>gh', gs.preview_hunk, { buffer = bufnr })
-    kmap('n', '<leader>gb', gs.blame_line)
   end
 })
 
