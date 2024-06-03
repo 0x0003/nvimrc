1d465555b9d06bfb2b5cce62604e56ffb468edf3
0x0003 <->
2024-06-03 11:30:28 +0900

    Switch to recent fidget.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -95,7 +95,7 @@ require('lazy').setup({
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
       -- status
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      { 'j-hui/fidget.nvim', opts = {} },
     },
     event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' }
   },
