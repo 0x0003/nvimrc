b2a2c371e1839aa2c3aebee5e13f5cd6b56cb5e3
0x0003 <->
2024-06-06 07:53:01 +0900

    Readme update

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 - neovim **0.10+**
 - make
 - gcc
+
+### Optional mason.nvim dependencies
+- `ghcup` for hls
+- `npm` for tsserver and html
+
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,7 +32,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local servers = {
-  hls = {}, -- NOTE: needs ghcup
+  hls = {},
 
   html = {
     filetypes = { 'html', 'twig', 'hbs' }
