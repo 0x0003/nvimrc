92b5e480d9ffa2f53f3277b82c69c68249001300
0x0003 <->
2024-07-04 13:33:37 +0900

    More custom icons

--- lua/plugins/mini-icons.lua
+++ lua/plugins/mini-icons.lua
@@ -1,24 +1,63 @@
-require('mini.icons').setup({
-  extension = {
-    lock = {
-      glyph = '󰌾',
-      hl = 'MiniIconsGreen'
-    }
-  },
+---@param t table
+---@return table
+local function conf_generic(t)
+  local r = {}
+  for _, a in ipairs(t) do
+    r[a] = { glyph = '' }
+  end
+  return r
+end
+local conf_t = conf_generic({
+  'conf', 'rc', 'cfg', 'ini'
+})
 
-  file = {
-    ['.ghci'] = {
-      glyph = '󰅩',
-      hl = 'MiniIconsPurple'
-    },
-    ['.envrc'] = {
-      glyph = '',
-      hl = 'MiniIconsGreen'
-    },
-    makefile = {
-      hl = 'MiniIconsGreen'
-    }
-  }
+local extension = {
+  -- common
+  xml = {
+    glyph = '󰗀',
+    hl = 'MiniIconsYellow'
+  },
+  -- shell scripts
+  fish = {
+    glyph = '󰈺',
+    hl = 'MiniIconsGreen'
+  },
+  sh = {
+    glyph = '󱆃',
+  },
+  -- misc
+  lock = {
+    glyph = '󰌾',
+    hl = 'MiniIconsGreen'
+  },
+  history = {
+    glyph = ''
+  },
+}
+for x, y in pairs(conf_t) do
+  extension[x] = y
+end
+
+local file = {
+  ['.ghci'] = {
+    glyph = '󰅩',
+    hl = 'MiniIconsPurple'
+  },
+  ['.envrc'] = {
+    glyph = '',
+    hl = 'MiniIconsGreen'
+  },
+  makefile = {
+    hl = 'MiniIconsGreen'
+  },
+  ['fish_variables'] = {
+    glyph = '󰻳',
+  },
+}
+
+require('mini.icons').setup({
+  extension = extension,
+  file = file,
 })
 
 MiniIcons.mock_nvim_web_devicons()
