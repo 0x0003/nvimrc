d94c8d756b6d13008101611864d88f1e943c0a62
0x0003 <->
2024-07-08 06:48:05 +0900

    Custom base16 gruvbox colorschemes

---
+++ lua/colors/gruvbox-dark-medium.lua
@@ -0,0 +1,22 @@
+vim.g.colors_name = 'base16-gruvbox-dark-medium'
+return {
+  base00 = '#282828',
+  base01 = '#3c3836',
+  base02 = '#5a524c',
+  base03 = '#7c6f64',
+  base04 = '#ebdbb2',
+  base05 = '#ddc7a1',
+  base06 = '#9d8e73',
+  base07 = '#fbf1c7',
+  base08 = '#ea6962',
+  base09 = '#e78a4e',
+  base0A = '#d8a657',
+  base0B = '#a9b665',
+  base0C = '#89b482',
+  base0D = '#7daea3',
+  base0E = '#d3869b',
+  base0F = '#998c71',
+  extra04 = '#928374',
+  extra09 = '#e1bb7e',
+  extra0D = '#9dc2ba'
+}
