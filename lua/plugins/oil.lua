6d1d2417124ecc971979f958233aea01069aeb29
0x0003 <0x0003@users.noreply.github.com>
2024-07-18 19:43:04 +0900

    Set all oil.nvim borders to "solid"

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,19 +1,22 @@
 local oil = require('oil')
 
 oil.setup({
+  default_file_explorer = true,
+
   view_options = {
     show_hidden = true,
     is_always_hidden = function(name)
       return name == '..'
     end,
   },
-  default_file_explorer = true,
+
   columns = {
     'icon',
     -- 'permissions',
     -- {'mtime', highlight = 'StatusCommand'},
     { 'size', highlight = 'Special' },
   },
+
   keymaps = {
     ['<BS>'] = 'actions.parent',
     ['<C-k>'] = 'actions.refresh',
@@ -22,10 +25,16 @@ oil.setup({
     ['<C-t>'] = 'actions.select_tab',
     ['<C-p>'] = 'actions.preview',
   },
+
+  float = { border = 'solid' },
+  preview = { border = 'solid' },
+  progress = { border = 'solid' },
+  ssh = { border = 'solid' },
+  keymaps_help = { border = 'solid' }
 })
 
 Kmap('n', '<leader>e', function()
-  return (vim.bo.ft ~= 'oil' and {oil.open()} or {oil.close()})[1]
-end,
+    return (vim.bo.ft ~= 'oil' and { oil.open() } or { oil.close() })[1]
+  end,
   'Oil: file explorer toggle')
 
